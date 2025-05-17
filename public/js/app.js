bf5b324b537d3a1386084c37a898b33a6aebeabc
funny <cuttix@hotmail.com>
2025-05-17 10:08:49 -0400

    Create app.js

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+// Main App Initialization
+document.addEventListener('DOMContentLoaded', () => {
+    // All initialization is handled by the individual modules
+    // (auth.js, chat.js, ui.js) which are loaded after this file
+});
